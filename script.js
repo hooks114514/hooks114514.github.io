23a7790b826c57b704a9583f752f76f559f40e6a
hooks114514 <165774433+hooks114514@users.noreply.github.com>
2024-11-03 18:09:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// script.js  
+const translations = {  
+    en: {  
+        title: "hooks1's website",  
+        link: "y bilibili personal space"  
+    },  
+    zh: {  
+        title: "hooks1的网站",  
+        link: "我的bilibili个人空间"  
+    }  
+};  
+  
+function changeLanguage(lang) {  
+    document.getElementById('title').textContent = translations[lang].title;  
+    document.getElementById('description').textContent = translations[lang].description;  
+}
 document.addEventListener('DOMContentLoaded', () => {  
     const navbarToggle = document.getElementById('navbar-toggle');  
     const navbarMenu = document.querySelector('.navbar-menu');  
